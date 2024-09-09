e7c8ffda3664fde3f4a79258dbef10fc2e065301
dravenk <longxianwen@outlook.com>
2024-09-09 17:00:49 +0800

    Update test

--- src/test/router_test.zig
+++ src/test/router_test.zig
@@ -5,7 +5,56 @@ const Route = zinc.Route;
 const Router = zinc.Router;
 const RouteError = Route.RouteError;
 
-test "router" {
+// test "router" {
+//     var router = Router.init(.{});
+//     const static_route = Route.init(.{ .method = .GET, .path = "/static" });
+//     try router.addRoute(static_route);
+
+//     const TestCase = struct {
+//         reqMethod: std.http.Method,
+//         reqPath: []const u8,
+//         expected: anyerror!Route,
+//     };
+//     const testCases = [_]TestCase{
+//         .{ .reqMethod = .GET, .reqPath = "/static", .expected = static_route },
+//         .{ .reqMethod = .GET, .reqPath = "/static/foo", .expected = static_route },
+//         .{ .reqMethod = .GET, .reqPath = "/static/foo/bar", .expected = static_route },
+//         .{ .reqMethod = .GET, .reqPath = "/static?code=123", .expected = static_route },
+//         .{ .reqMethod = .GET, .reqPath = "/static?code=123&state=xyz#foo", .expected = static_route },
+
+//         .{ .reqMethod = .POST, .reqPath = "/static", .expected = RouteError.MethodNotAllowed },
+
+//         .{ .reqMethod = .GET, .reqPath = "/foo/static", .expected = RouteError.NotFound },
+//         .{ .reqMethod = .GET, .reqPath = "/foo/static/bar", .expected = RouteError.NotFound },
+//         .{ .reqMethod = .GET, .reqPath = "/foo/static/hello.css", .expected = RouteError.NotFound },
+//     };
+
+//     for (testCases, 0..) |tc, i| {
+//         std.debug.print(" \r\n test case {d}, path: {s} ", .{ i, tc.reqPath });
+//         const route_expected = router.matchRoute(tc.reqMethod, tc.reqPath) catch |err| {
+//             try testing.expect(err == (tc.expected catch |e| e));
+//             // std.debug.print(" \r\n test1 case {d} passed, path: {s} ", .{ i, tc.reqPath });
+//             continue;
+//         };
+
+//         try testing.expectEqual(route_expected.*, (try tc.expected));
+//         // std.debug.print(" \r\n test2 case {d} passed, path: {s} ", .{ i, tc.reqPath });
+//     }
+// }
+
+test "root page" {
+    var router = Router.init(.{});
+    const route = Route.init(.{ .method = .GET, .path = "/" });
+    try router.addRoute(route);
+
+    const req = zinc.Request.init(.{ .method = .GET, .path = "/" });
+    const res = zinc.Response.init(.{});
+    const ctx = zinc.Context.init(.{ .request = req, .response = res });
+
+    try router.handleContext(ctx);
+    try testing.expectEqual(res.statusCode, 200);
+}
+test "router, routeTree and router.getRoute" {
     var router = Router.init(.{});
     const static_route = Route.init(.{ .method = .GET, .path = "/static" });
     try router.addRoute(static_route);
@@ -17,75 +66,24 @@ test "router" {
     };
     const testCases = [_]TestCase{
         .{ .reqMethod = .GET, .reqPath = "/static", .expected = static_route },
-        .{ .reqMethod = .GET, .reqPath = "/static/foo", .expected = static_route },
-        .{ .reqMethod = .GET, .reqPath = "/static/foo/bar", .expected = static_route },
         .{ .reqMethod = .GET, .reqPath = "/static?code=123", .expected = static_route },
         .{ .reqMethod = .GET, .reqPath = "/static?code=123&state=xyz#foo", .expected = static_route },
 
         .{ .reqMethod = .POST, .reqPath = "/static", .expected = RouteError.MethodNotAllowed },
-
+        .{ .reqMethod = .GET, .reqPath = "/static/foo", .expected = RouteError.NotFound },
+        .{ .reqMethod = .GET, .reqPath = "/static/foo/bar", .expected = RouteError.NotFound },
         .{ .reqMethod = .GET, .reqPath = "/foo/static", .expected = RouteError.NotFound },
         .{ .reqMethod = .GET, .reqPath = "/foo/static/bar", .expected = RouteError.NotFound },
         .{ .reqMethod = .GET, .reqPath = "/foo/static/hello.css", .expected = RouteError.NotFound },
     };
 
     for (testCases, 0..) |tc, i| {
-        std.debug.print(" \r\n test case {d}, path: {s} ", .{ i, tc.reqPath });
-        const route_expected = router.matchRoute(tc.reqMethod, tc.reqPath) catch |err| {
-            try testing.expect(err == (tc.expected catch |e| e));
-            // std.debug.print(" \r\n test1 case {d} passed, path: {s} ", .{ i, tc.reqPath });
-            continue;
-        };
-
-        try testing.expectEqual(route_expected.*, (try tc.expected));
-        // std.debug.print(" \r\n test2 case {d} passed, path: {s} ", .{ i, tc.reqPath });
-    }
-}
-
-test "routeTree" {
-    var router = Router.init(.{});
-    const static_route = Route.init(.{ .method = .GET, .path = "/static" });
-    try router.addRoute(static_route);
-
-    const TestCase = struct {
-        reqMethod: std.http.Method,
-        reqPath: []const u8,
-        expected: anyerror!Route,
-    };
-    const testCases = [_]TestCase{
-        .{ .reqMethod = .GET, .reqPath = "/static", .expected = static_route },
-        .{ .reqMethod = .GET, .reqPath = "/static?code=123", .expected = static_route },
-        .{ .reqMethod = .GET, .reqPath = "/static?code=123&state=xyz#foo", .expected = static_route },
-
-        .{ .reqMethod = .POST, .reqPath = "/static", .expected = RouteError.MethodNotAllowed },
-
-        .{ .reqMethod = .GET, .reqPath = "/static/foo", .expected = RouteError.NotFound },
-        .{ .reqMethod = .GET, .reqPath = "/static/foo/bar", .expected = RouteError.NotFound },
-        .{ .reqMethod = .GET, .reqPath = "/foo/static", .expected = RouteError.NotFound },
-        .{ .reqMethod = .GET, .reqPath = "/foo/static/bar", .expected = RouteError.NotFound },
-        // .{ .reqMethod = .GET, .reqPath = "/foo/static/hello.css", .expected = RouteError.NotFound },
-    };
-
-    for (testCases, 0..) |tc, i| {
-        std.debug.print(" \r\n routeTree test case {d}, path: {s} \r\n", .{ i, tc.reqPath });
-
-        const rTree = router.getRouteTree(tc.reqPath) catch |err| {
-            std.debug.print(" \r\n 111 routeTree test case {d} passed, path: {s} ", .{ i, tc.reqPath });
-
+        std.debug.print(" \r\n routeTree test case {d}, path: {s}", .{ i, tc.reqPath });
+        const rTree_route = router.getRoute(tc.reqMethod, tc.reqPath) catch |err| {
             try testing.expect(err == (tc.expected catch |e| e));
             continue;
         };
 
-        _ = rTree.route;
-        std.debug.print(" \r\n 222 routeTree test case {d} passed, path: {s} ", .{ i, tc.reqPath });
-
-        // // // const route = router.getRoute(tc.reqMethod, tc.reqPath) catch |err| {
-        // // //     // std.debug.print(" \r\n routeTree test case {d} failed, err: {any} ", .{ i, err });
-        // // //     try testing.expect(err == (tc.expected catch |e| e));
-        // // //     continue;
-        // // // };
-        // std.debug.print(" \r\n routeTree test case {d} passed, path: {any}\r\n", .{ i, router.routes.items[0] });
-        // try testing.expectEqual(route.*, (try tc.expected));
-        // std.debug.print(" \r\n routeTree test case {d} passed, path: {s} ", .{ i, tc.reqPath });
+        try testing.expectEqual(rTree_route.*, (try tc.expected));
     }
 }
